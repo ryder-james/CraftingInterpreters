8efcbcd1a69ef2d3ab4f086557026b83a71f2873
Ryder James <work.jamesr@gmail.com>
2026-07-08 13:47:24 -0600

    feat: RuntimeError on division by zero

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -52,6 +52,9 @@ class Interpreter implements Visitor<Object> {
 			throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
 		case SLASH:
 			checkNumberOperands(expr.operator, left, right);
+			if ((double)right == 0.0) {
+				throw new RuntimeError(expr.operator, "Cannot divide by 0.");
+			}
 			return (double)left / (double)right;
 		case STAR:
 			checkNumberOperands(expr.operator, left, right);
