1530d1a5445444965aaf592887fe1149623bce27
Ryder James <work.jamesr@gmail.com>
2026-07-08 13:40:59 -0600

    style: Normalize space indents to tabs

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -11,73 +11,73 @@ import java.util.List;
 
 public class Lox {
 	private static final Interpreter interpreter = new Interpreter();
-    static boolean hadError = false;
-    static boolean hadRuntimeError = false;
-    
-    public static void main(String[] args) throws IOException {
-        if (args.length > 1) {
-            System.out.print("Usage: jlox [script]");
-            System.exit(64);
-        } else if (args.length == 1) {
-            runFile(args[0]);
-        } else {
-            runPrompt();
-        }
-    }
+	static boolean hadError = false;
+	static boolean hadRuntimeError = false;
+	
+	public static void main(String[] args) throws IOException {
+		if (args.length > 1) {
+			System.out.print("Usage: jlox [script]");
+			System.exit(64);
+		} else if (args.length == 1) {
+			runFile(args[0]);
+		} else {
+			runPrompt();
+		}
+	}
 
-    private static void runFile(String path) throws IOException {
-        byte[] bytes = Files.readAllBytes(Paths.get(path));
-        run(new String(bytes, Charset.defaultCharset()));
+	private static void runFile(String path) throws IOException {
+		byte[] bytes = Files.readAllBytes(Paths.get(path));
+		run(new String(bytes, Charset.defaultCharset()));
 
-        // Indicate an error in the exit code.
-        if (hadError) System.exit(65);
-        if (hadRuntimeError) System.exit(70);
-    }
+		// Indicate an error in the exit code.
+		if (hadError) System.exit(65);
+		if (hadRuntimeError) System.exit(70);
+	}
 
-    private static void runPrompt() throws IOException {
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(input);
+	private static void runPrompt() throws IOException {
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(input);
 
-        for (;;) {
-            System.out.print("> ");
-            String line = reader.readLine();
-            if (line == null) break;
-            run(line);
-            hadError = false;
-        }
-    }
+		for (;;) {
+			System.out.print("> ");
+			String line = reader.readLine();
+			if (line == null) break;
+			run(line);
+			hadError = false;
+		}
+	}
 
-    private static void run(String source) {
-        Scanner scanner = new Scanner(source);
-        List<Token> tokens = scanner.scanTokens();
-        Parser parser = new Parser(tokens);
-        Expr expression = parser.parse();
-        
-        if (hadError) return;
+	private static void run(String source) {
+		Scanner scanner = new Scanner(source);
+		List<Token> tokens = scanner.scanTokens();
+		Parser parser = new Parser(tokens);
+		Expr expression = parser.parse();
+		
+		if (hadError) return;
 
-        interpreter.interpret(expression);
-    }
+		interpreter.interpret(expression);
+	}
 
-    static void error(int line, String message) {
-        report(line, "", message);
-    }
-    
-    static void error(Token token, String message) {
-    	if (token.type == TokenType.EOF) {
-    		report(token.line, " at end", message);
-    	} else {
-    		report(token.line, " at '" + token.lexeme + "'", message);
-    	}
-    }
-    
-    static void runtimeError(RuntimeError error) {
-    	System.err.println(error.getMessage() +
-    			"\n[line " + error.token.line + "]");
-    	hadRuntimeError = true;
-    }
+	static void error(int line, String message) {
+		report(line, "", message);
+	}
+	
+	static void error(Token token, String message) {
+		if (token.type == TokenType.EOF) {
+			report(token.line, " at end", message);
+		} else {
+			report(token.line, " at '" + token.lexeme + "'", message);
+		}
+	}
+	
+	static void runtimeError(RuntimeError error) {
+		System.err.println(error.getMessage() +
+				"\n[line " + error.token.line + "]");
+		hadRuntimeError = true;
+	}
 
-    private static void report(int line, String where, String message) {
-        System.err.println("[line " + line + "] Error" + where + ": " + message);
-        hadError = true;
-    }
+	private static void report(int line, String where, String message) {
+		System.err.println("[line " + line + "] Error" + where + ": " + message);
+		hadError = true;
+	}
 }
--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -13,10 +13,10 @@ public class GenerateAst {
 		}
 		String outputDir = args[0];
 		defineAst(outputDir, "Expr", Arrays.asList(
-			"Binary     : Expr left, Token operator, Expr right",
-			"Grouping   : Expr expression",
-			"Literal    : Object value",
-			"Unary      : Token operator, Expr right"
+			"Binary		: Expr left, Token operator, Expr right",
+			"Grouping	: Expr expression",
+			"Literal	: Object value",
+			"Unary		: Token operator, Expr right"
 		));
 	}
 
