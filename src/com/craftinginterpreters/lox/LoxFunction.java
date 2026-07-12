9365cf0a0437af87ad5a4b85f98fac530de5ddfe
Ryder James <work.jamesr@gmail.com>
2026-07-12 00:24:13 -0600

    fix: Prevent returning from init
    except early returns, which just return "this"

--- src/com/craftinginterpreters/lox/LoxFunction.java
+++ src/com/craftinginterpreters/lox/LoxFunction.java
@@ -5,16 +5,21 @@ import java.util.List;
 class LoxFunction implements LoxCallable {
 	private final Stmt.Function declaration;
 	private final Environment closure;
+
+	private final boolean isInitializer;
 	
-	LoxFunction(Stmt.Function declaration, Environment closure) {
+	LoxFunction(Stmt.Function declaration, Environment closure,
+			boolean isInitializer)
+	{
 		this.declaration = declaration;
 		this.closure = closure;
+		this.isInitializer = isInitializer;
 	}
 
 	LoxFunction bind(LoxInstance instance) {
 		Environment env = new Environment(closure);
 		env.define("this", instance);
-		return new LoxFunction(declaration, env);
+		return new LoxFunction(declaration, env, isInitializer);
 	}
 
 	@Override
@@ -32,9 +37,12 @@ class LoxFunction implements LoxCallable {
 		try {
 			interpreter.executeBlock(declaration.body, env);
 		} catch (Return returnValue) {
+			if (isInitializer) return closure.getAt(0, "this");
+
 			return returnValue.value;
 		}
 		
+		if (isInitializer) return closure.getAt(0, "this");
 		return null;
 	}
 	
